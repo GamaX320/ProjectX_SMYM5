7856b527a7f44aaf8609c40eb7c2a6f0acc24ea6
GamaX320 <bryan.x320@gmail.com>
2015-03-11 23:03:43 +0800

    Third commit (add test fragment)

---
+++ app/src/main/java/com/example/bryanty/projectx_smym5/TestFragment.java
@@ -0,0 +1,27 @@
+package com.example.bryanty.projectx_smym5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by BRYANTY on 11/03/2015.
+ */
+public class TestFragment extends Fragment {
+    View rootView;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_test, container, false);
+        return rootView;
+    }
+}
