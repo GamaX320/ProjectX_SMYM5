1e32a3a6e9884ec0176e1c448fcf386cf17c1349
GamaX320 <bryan.x320@gmail.com>
2015-03-14 17:41:59 +0800

    Create account and expense table

--- app/src/main/java/com/example/bryanty/projectx_smym5/DBHandler.java
+++ app/src/main/java/com/example/bryanty/projectx_smym5/DBHandler.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.bryanty.projectx_smym5.domain.Account;
+import com.example.bryanty.projectx_smym5.domain.Expense;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +20,19 @@ public class DBHandler extends SQLiteOpenHelper{
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "financial.db";
     public static final String TABLE_NAME ="accounts";
+    public static final String TABLE_NAME2 ="expense";
 
+    //column for table account
     public static final String COLUMN_ACC_ID ="accID";
     public static final String COLUMN_ACC_NAME ="accName";
     public static final String COLUMN_ACC_COLOR ="accColor";
     public static final String COLUMN_ACC_AMOUNT ="accAmount";
+
+    //column for table expense
+    public static final String COLUMN_EXP_ID ="expID";
+    public static final String COLUMN_EXP_TYPE ="expType";
+    public static final String COLUMN_EXP_AMOUNT ="expAmount";
+    public static final String COLUMN_EXP_ACC_ID ="accID";
 
     public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
@@ -34,24 +43,36 @@ public class DBHandler extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //query for create table account
         String query= "CREATE TABLE " + TABLE_NAME + "(" +
                 COLUMN_ACC_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_ACC_NAME + " TEXT," +
                 COLUMN_ACC_COLOR + " INTEGER," +
                 COLUMN_ACC_AMOUNT + " DOUBLE);";
 
+        //query for create table expense
+        String query2= "CREATE TABLE " + TABLE_NAME2 + "(" +
+                COLUMN_EXP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_EXP_TYPE + " TEXT," +
+                COLUMN_EXP_AMOUNT + " DOUBLE," +
+                COLUMN_EXP_ACC_ID + " INTEGER, FOREIGN KEY ("+COLUMN_EXP_ACC_ID +") REFERENCES "+TABLE_NAME +" ("+COLUMN_ACC_ID +"));";
+
         db.execSQL(query);
+        db.execSQL(query2);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String query= "DROP TABLE IF EXISTS " + TABLE_NAME;
+        String query2= "DROP TABLE IF EXISTS " + TABLE_NAME2;
+
         db.execSQL(query);
+        db.execSQL(query2);
 
         onCreate(db);
     }
 
-    //add a new row to database
+    //add a new account to database
     public void addAccount(Account acoount){
         ContentValues values = new ContentValues();
         values.put(COLUMN_ACC_NAME, acoount.get_accName());
@@ -62,13 +83,13 @@ public class DBHandler extends SQLiteOpenHelper{
         db.close();
     }
 
-    //delete a row from database
+    //delete a account from database
     public void deleteAccount(String accountName){
         SQLiteDatabase db= getWritableDatabase();
         db.execSQL("DELETE FROM  "+TABLE_NAME +" WHERE "+COLUMN_ACC_NAME + "=\"" +accountName +"\";");
     }
 
-    //retrieve all row from database
+    //retrieve all account from database
     public List<Account> getAllAccount(){
         String query="SELECT * FROM "+TABLE_NAME +" WHERE 1";
         SQLiteDatabase db= getWritableDatabase();
@@ -86,6 +107,50 @@ public class DBHandler extends SQLiteOpenHelper{
                 userRecord.set_accName(cursor.getString(1));
                 userRecord.set_accColor(cursor.getInt(2));
                 userRecord.set_accAmount(cursor.getDouble(3));
+
+                records.add(userRecord);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return records;
+    }
+//============================================================================================================================================================
+    //add a new expense to database
+    public void addExpense(Expense expense){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_EXP_TYPE, expense.get_expType());
+        values.put(COLUMN_EXP_AMOUNT, expense.get_expAmount());
+        values.put(COLUMN_EXP_ACC_ID, expense.get_accID());
+        SQLiteDatabase db= getWritableDatabase();
+        db.insert(TABLE_NAME2,null,values);
+        db.close();
+    }
+
+    //delete a expense from database
+    public void deleteExpense(int expenseID){
+        SQLiteDatabase db= getWritableDatabase();
+        db.execSQL("DELETE FROM  "+TABLE_NAME2 +" WHERE "+COLUMN_EXP_ID + "=\"" +expenseID +"\";");
+    }
+
+    //retrieve all expense from database
+    public List<Expense> getAllExpense(){
+        String query="SELECT * FROM "+TABLE_NAME2 +" WHERE 1";
+        SQLiteDatabase db= getWritableDatabase();
+
+        List<Expense> records = new ArrayList<Expense>();
+        //cursor point to your location
+        Cursor cursor = db.rawQuery(query, null);
+        //move to first row in your result
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()){
+            if(cursor.getString(cursor.getColumnIndex("accName")) != null){
+                Expense userRecord = new Expense();
+                userRecord.set_expID(cursor.getInt(0));
+                userRecord.set_expType(cursor.getString(1));
+                userRecord.set_expAmount(cursor.getDouble(2));
+                userRecord.set_accID(cursor.getInt(3));
 
                 records.add(userRecord);
                 cursor.moveToNext();
---
+++ app/src/main/java/com/example/bryanty/projectx_smym5/domain/Expense.java
@@ -0,0 +1,69 @@
+package com.example.bryanty.projectx_smym5.domain;
+
+/**
+ * Created by BRYANTY on 14/03/2015.
+ */
+public class Expense {
+    private int _expID;
+    private String _expType;
+    private double _expAmount;
+    private int _accID;
+
+    public Expense() {
+    }
+
+    public Expense(String _expType, double _expAmount, int _accID) {
+        this._expType = _expType;
+        this._expAmount = _expAmount;
+        this._accID = _accID;
+    }
+
+    public Expense(int _expID, String _expType, double _expAmount, int _accID) {
+        this._expID = _expID;
+        this._expType = _expType;
+        this._expAmount = _expAmount;
+        this._accID = _accID;
+    }
+
+    public int get_expID() {
+        return _expID;
+    }
+
+    public void set_expID(int _expID) {
+        this._expID = _expID;
+    }
+
+    public String get_expType() {
+        return _expType;
+    }
+
+    public void set_expType(String _expType) {
+        this._expType = _expType;
+    }
+
+    public double get_expAmount() {
+        return _expAmount;
+    }
+
+    public void set_expAmount(double _expAmount) {
+        this._expAmount = _expAmount;
+    }
+
+    public int get_accID() {
+        return _accID;
+    }
+
+    public void set_accID(int _accID) {
+        this._accID = _accID;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "_expID=" + _expID +
+                ", _expType='" + _expType + '\'' +
+                ", _expAmount=" + _expAmount +
+                ", _accID=" + _accID +
+                '}';
+    }
+}
